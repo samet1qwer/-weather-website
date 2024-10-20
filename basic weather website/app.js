16e13b90506957e28340f4fa3a77486486b62794
CyberTempest1w <samet102a@gmail.com>
2024-10-20 12:59:13 +0300

    basic weather website
    A simple website that brings the weather using API

---
+++ basic weather website/app.js
@@ -0,0 +1,75 @@
+const şhr = document.querySelector("#şehr")
+const hava = document.querySelector("#hava")
+const sicaklik = document.querySelector("#sicaklik")
+const hissedilen = document.querySelector("#hissedilen")
+const nem = document.querySelector("#nem")
+const rüzgar = document.querySelector("#ruzgar")
+
+
+const konum=document.getElementById("şehir")
+const buton=document.getElementById("get")
+
+const api = "413599eb33e96aad87e49e9f03891ac5"
+
+const country = "TR"
+
+function getCoordinates(apiKey, cityName, countryCode) {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${countryCode}&appid=${apiKey}`)
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.length > 0) {
+                const { lat, lon } = data[0]
+                getWeather(apiKey, lat, lon)
+            } else {
+                console.log("Verilen şehir için veri bulunamadı.")
+            }
+        })
+        .catch((err) => console.log(err))
+}
+
+function getWeather(apiKey, lon, lat) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lon}&lon=${lat}&appid=${apiKey}&lang=tr`)
+        .then((response) => response.json())
+        .then((weatherData) => {
+            displayWeather(weatherData)
+            
+        })
+        .catch((err) => console.log(err))
+}
+
+function displayWeather(data) {
+    const city = data.name
+    const country = data.sys.country
+    const description = data.weather[0].description
+    const temperature = (data.main.temp - 273.15).toFixed(1)
+    const feelsLike = (data.main.feels_like - 273.15).toFixed(1)
+    const humidity = data.main.humidity
+  
+    const windSpeed = data.wind.speed
+
+    
+    console.log(description)
+
+    şhr.innerHTML=`Şehir: ${city}, ${country}`
+    hava.innerHTML=`Hava Durumu: ${description}`
+    sicaklik.innerHTML=`Sıcaklık: ${temperature}°C`
+    hissedilen.innerHTML=`Hissedilen Sıcaklık: ${feelsLike}°C`
+    nem.innerHTML=`Nem: ${humidity}%`
+    rüzgar.innerHTML=`Rüzgar Hızı: ${windSpeed} m/s`
+    
+    // if (description=="açık") {
+    //     const containr=document.querySelector(".container")
+    //     console.log("başarılı")
+    //     console.log(containr)
+        
+         
+    // }
+}
+buton.addEventListener("click",()=>{
+    const city = konum.value.toUpperCase()    
+    getCoordinates(api,city,country)
+
+})
+
+
+
